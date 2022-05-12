f9afdb09477d8991009f99471f3c37967f91d506
DaliGabriel <21015405@es.uveg.edu.mx>
2022-05-11 23:45:18 -0500

    register init

--- controller/create_user.php
+++ controller/create_user.php
@@ -13,9 +13,26 @@ if (!empty($_POST['first_name_register'])
     !empty($_POST['password_register_confirm'])
     ) 
     {
-    # code...
+        $name = $_POST['first_name_register'];
+        $l_name = $_POST['last_name_register'];
+        $mail = $_POST['email_register'];
+        $pass = $_POST['password_register_confirm'];
+    
+        $sql = "INSERT INTO register (name, last_name, email, password) VALUES(?,?,?,?)";
+        $stmt = $conn -> prepare($sql);
+        $pass = password_hash($pass,PASSWORD_BCRYPT);
+        $stmt->bind_param("ssss",$name,$l_name,$mail,$pass);
+
+
+        if ($stmt -> execute()) {
+            $message = "Created User";
+            echo $message;
+        }else{
+            $message ="cant create user";
+            echo $message;
+        }
 
 }
 
 
-header('location: ../views/login.html');
+header('location: ../views/login.php');
